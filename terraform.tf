edb633628e0045bd7f75e45c6baa17c0888a67f4
menanceci <lordi@abv.bg>
2024-07-17 14:09:56 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,14 +3,14 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/*  cloud {
+    organization = "Test_environment"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
